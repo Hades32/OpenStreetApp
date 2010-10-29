a2188da27fb9093b66551713cc4959b7d3375f24
Martin Rauscher <hadesxxl@gmx.de>
2010-10-29 16:44:28 +0200

    Added OSMControl and TileDownloadManager
    Signed-off-by: Martin Rauscher <hadesxxl@gmx.de>

---
+++ OpenStreetApp/OSMControl.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace OpenStreetApp
+{
+    public partial class OSMControl : UserControl
+    {
+        #region MapCenter
+
+        /// <summary>
+        /// MapCenter Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty MapCenterProperty =
+            DependencyProperty.Register("MapCenter", typeof(Point), typeof(OSMControl),
+                new PropertyMetadata((Point)new Point(48.6, 10.1),
+                    new PropertyChangedCallback(OnMapCenterChanged)));
+
+        /// <summary>
+        /// Gets or sets the MapCenter property. This dependency property 
+        /// indicates the coordinates of the map center.
+        /// </summary>
+        public Point MapCenter
+        {
+            get { return (Point)GetValue(MapCenterProperty); }
+            set { SetValue(MapCenterProperty, value); }
+        }
+
+        /// <summary>
+        /// Handles changes to the MapCenter property.
+        /// </summary>
+        private static void OnMapCenterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            OSMControl target = (OSMControl)d;
+            Point oldValue = (Point)e.OldValue;
+            Point newValue = target.MapCenter;
+            target.OnMapCenterChanged(oldValue, newValue);
+        }
+
+        /// <summary>
+        /// Provides derived classes an opportunity to handle changes to the MapCenter property.
+        /// </summary>
+        protected virtual void OnMapCenterChanged(Point oldValue, Point newValue)
+        {
+            this.OnMapViewChanged();
+        }
+
+        #endregion
+
+        #region ZoomLevel
+
+        /// <summary>
+        /// ZoomLevel Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty ZoomLevelProperty =
+            DependencyProperty.Register("ZoomLevel", typeof(double), typeof(OSMControl),
+                new PropertyMetadata((double)14.0,
+                    new PropertyChangedCallback(OnZoomLevelChanged)));
+
+        /// <summary>
+        /// Gets or sets the ZoomLevel property. This dependency property 
+        /// indicates the zoom level. For non-integer values the pictures are scaled appropiately.
+        /// </summary>
+        public double ZoomLevel
+        {
+            get { return (double)GetValue(ZoomLevelProperty); }
+            set { SetValue(ZoomLevelProperty, value); }
+        }
+
+        /// <summary>
+        /// Handles changes to the ZoomLevel property.
+        /// </summary>
+        private static void OnZoomLevelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            OSMControl target = (OSMControl)d;
+            double oldValue = (double)e.OldValue;
+            double newValue = target.ZoomLevel;
+            target.OnZoomLevelChanged(oldValue, newValue);
+        }
+
+        /// <summary>
+        /// Provides derived classes an opportunity to handle changes to the ZoomLevel property.
+        /// </summary>
+        protected virtual void OnZoomLevelChanged(double oldValue, double newValue)
+        {
+            this.OnMapViewChanged();
+        }
+
+        #endregion
+
+        TileDownloadManager tileDownloader;
+
+        //TODO: other data structure?
+        Image[] imgs = new Image[40];
+        public OSMControl(TileDownloadManager tdm)
+        {
+            InitializeComponent();
+
+            this.tileDownloader = tdm;
+
+            //add Images
+            for (int i = 0; i < imgs.Length; i++)
+            {
+                imgs[i] = new Image();
+                imgs[i].Width = 256;
+                imgs[i].Height = 256;
+                this.LayoutRoot.Children.Add(imgs[i]);
+            }
+        }
+
+        protected virtual void OnMapViewChanged()
+        {
+            this.tileDownloader.fetch(this.MapCenter, this.ZoomLevel, (ImageSource img) => { });
+        }
+    }
+}
---
+++ OpenStreetApp/TileDownloadManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media;
+
+namespace OpenStreetApp
+{
+    class TileDownloadManager
+    {
+        /// <summary>
+        /// This method downloads the image tile for the given coordinates and the given zoom level from the server 
+        /// and then calls a callback function with a ImageSource created from the tile.
+        /// this method is asynchronous
+        /// </summary>
+        /// <param name="coord">The world coordinates</param>
+        /// <param name="zoom">The zoom level</param>
+        /// <param name="callback">The callback function</param>
+        public void fetch(Point coord, int zoom, Action<ImageSource> callback)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(fetch_async));
+            tdm_task task;
+            task.coord = coord;
+            task.zoom = zoom;
+            task.callback = callback;
+            t.Start(task);
+        }
+
+        private void fetch_async(object obj)
+        {
+            var task = (tdm_task)obj;
+        }
+
+        private struct tdm_task
+        {
+            public Point coord;
+            public int zoom;
+            public Action<ImageSource> callback;
+        }
+    }
+}
