6a69f2d5a7a63086ccd4b45253d87d558598fdbd
chillta <chillta@web.de>
2010-11-02 14:08:46 +0100

    First implementation of fetch_async Added synchronized Collection TCollection.cs Cleaned MainPage.xaml.cs
    Signed-off-by: chillta <chillta@web.de>

--- OpenStreetApp/MainPage.xaml.cs
+++ OpenStreetApp/MainPage.xaml.cs
@@ -20,34 +20,6 @@ namespace OpenStreetApp
         public MainPage()
         {
             InitializeComponent();
-
-        }
-
-        private void loadButton_Click(object sender, EventArgs e)
-        {
-            Point p = WorldToTilePos(9.952306, 48.424335, 16);
-            this.picture.Source = new BitmapImage(new Uri("http://tile.openstreetmap.org/16/" + ((int) p.X) + "/" + ((int) p.Y) + ".png"));
-        }
-
-        public Point WorldToTilePos(double lon, double lat, int zoom)
-        {
-            Point p = new Point();
-            p.X = (float)((lon + 180.0) / 360.0 * (1 << zoom));
-            p.Y = (float)((1.0 - Math.Log(Math.Tan(lat * Math.PI / 180.0) +
-                1.0 / Math.Cos(lat * Math.PI / 180.0)) / Math.PI) / 2.0 * (1 << zoom));
-           
-            return p;
-        }
-
-        public Point TileToWorldPos(double tile_x, double tile_y, int zoom)
-        {
-            Point p = new Point();
-            double n = Math.PI - ((2.0 * Math.PI * tile_y) / Math.Pow(2.0, zoom));
-
-            p.X = (float)((tile_x / Math.Pow(2.0, zoom) * 360.0) - 180.0);
-            p.Y = (float)(180.0 / Math.PI * Math.Atan(Math.Sinh(n)));
-
-            return p;
         }
     }
 }
--- OpenStreetApp/TileDownloadManager.cs
+++ OpenStreetApp/TileDownloadManager.cs
@@ -2,11 +2,27 @@
 using System.Threading;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.IsolatedStorage;
+using System.IO;
 
 namespace OpenStreetApp
 {
     public class TileDownloadManager
     {
+        private static TCollection<String> dictionary = new TCollection<String>();
+        private static IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication();
+
+        static TileDownloadManager()
+        {
+            if (!isf.DirectoryExists("TileCache"))
+            {
+                isf.CreateDirectory("TileCache");
+            }
+        }
+
         /// <summary>
         /// This method downloads the image tile for the given coordinates and the given zoom level from the server 
         /// and then calls a callback function with a ImageSource created from the tile.
@@ -28,6 +44,41 @@ namespace OpenStreetApp
         private void fetch_async(object obj)
         {
             var task = (tdm_task)obj;
+            var p = WorldToTilePos(task.coord.X, task.coord.Y, task.zoom);
+            BitmapSource bms;
+
+            if (dictionary.containsSynchronized(p.X + "" + p.Y + "" + task.zoom))
+            {
+                bms = new BitmapImage(new Uri(Path.Combine("TileCache", p.X + "" + p.Y + "" + task.zoom + ".png")));
+            }
+            else
+            {
+                bms = new BitmapImage(new Uri("http://tile.openstreetmap.org/" + task.zoom + "/" + ((int)task.coord.X) + "/" + ((int)task.coord.Y) + ".png"));
+                isf.CreateFile(Path.Combine("TileCache", p.X + "" + p.Y + "" + task.zoom + ".png"));
+                dictionary.addSynchronized(p.X + "" + p.Y + "" + task.zoom);
+            }
+            task.callback(bms);
+        }
+
+        public Point WorldToTilePos(double lon, double lat, int zoom)
+        {
+            Point p = new Point();
+            p.X = (float)((lon + 180.0) / 360.0 * (1 << zoom));
+            p.Y = (float)((1.0 - Math.Log(Math.Tan(lat * Math.PI / 180.0) +
+                1.0 / Math.Cos(lat * Math.PI / 180.0)) / Math.PI) / 2.0 * (1 << zoom));
+
+            return p;
+        }
+
+        public Point TileToWorldPos(double tile_x, double tile_y, int zoom)
+        {
+            Point p = new Point();
+            double n = Math.PI - ((2.0 * Math.PI * tile_y) / Math.Pow(2.0, zoom));
+
+            p.X = (float)((tile_x / Math.Pow(2.0, zoom) * 360.0) - 180.0);
+            p.Y = (float)(180.0 / Math.PI * Math.Atan(Math.Sinh(n)));
+
+            return p;
         }
 
         private struct tdm_task
