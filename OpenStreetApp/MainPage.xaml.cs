63834ed424fc62d1fdf1181bc42c51ef9f83dfa2
Martin Rauscher <hadesxxl@gmx.de>
2010-11-07 17:41:51 +0100

    Changed implementation to use the DeepZoom technology. (MultiScaleImage)
    Signed-off-by: Martin Rauscher <hadesxxl@gmx.de>

--- OpenStreetApp/MainPage.xaml.cs
+++ OpenStreetApp/MainPage.xaml.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Reactive;
 
 namespace OpenStreetApp
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Konstruktor
+        Point lastMousePos = new Point();
+        Point lastMouseLogicalPos = new Point();
+        Point lastMouseViewPort = new Point();
+        bool duringDrag = false;
+        double zoom = 1;
+
         public MainPage()
         {
             InitializeComponent();
+
+            this.OSM_Map.Source = new OSMTileSource();
+
+            var down = Microsoft.Phone.Reactive.Observable.FromEvent<MouseButtonEventArgs>(this.OSM_Map, "MouseLeftButtonDown");
+            var up = Microsoft.Phone.Reactive.Observable.FromEvent<MouseButtonEventArgs>(this.OSM_Map, "MouseLeftButtonUp");
+            var downAndUp = down.Zip(up, (handler, event_arg) => event_arg);
+
+            // das funzt noch nicht! (richtig)
+            var x = downAndUp.Timestamp().BufferWithCount(2).SubscribeOnDispatcher().Subscribe(
+                eventList =>
+                {
+                    // zoom if it was actually a double click and not just two clicks
+                    if (eventList[1].Timestamp - eventList[0].Timestamp < TimeSpan.FromSeconds(2))
+                    {
+                        var newzoom = zoom / 1.3;
+                        Point logicalPoint = this.OSM_Map.ElementToLogicalPoint(this.lastMousePos);
+                        this.OSM_Map.ZoomAboutLogicalPoint(zoom / newzoom, logicalPoint.X, logicalPoint.Y);
+                        zoom = newzoom;
+                    }
+                });
         }
 
         public void TestTiles(Point coord, double zoom, string path)
@@ -51,6 +78,31 @@ namespace OpenStreetApp
         private void POIButton_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void OSM_Map_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            lastMouseLogicalPos = e.GetPosition(this.OSM_Map);
+            lastMouseViewPort = this.OSM_Map.ViewportOrigin;
+            duringDrag = true;
+        }
+
+        private void OSM_Map_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            duringDrag = false;
+            this.OSM_Map.UseSprings = true;
+        }
+
+        private void OSM_Map_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (duringDrag)
+            {
+                Point newPoint = lastMouseViewPort;
+                Point thisMouseLogicalPos = e.GetPosition(this.OSM_Map);
+                newPoint.X += (lastMouseLogicalPos.X - thisMouseLogicalPos.X) / this.OSM_Map.ActualWidth * this.OSM_Map.ViewportWidth;
+                newPoint.Y += (lastMouseLogicalPos.Y - thisMouseLogicalPos.Y) / this.OSM_Map.ActualWidth * this.OSM_Map.ViewportWidth;
+                this.OSM_Map.ViewportOrigin = newPoint;
+            }
         }
     }
 }
---
+++ OpenStreetApp/OSMTileSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace OpenStreetApp
+{
+    public class OSMTileSource : MultiScaleTileSource
+    {
+        public OSMTileSource()
+            : base(0x8000000, 0x8000000, 256, 256, 0) //no idea why 0x8000000, but it works :)
+        {
+
+        }
+
+        protected override void GetTileLayers(int tileLevel, int tilePositionX, int tilePositionY, System.Collections.Generic.IList<object> tileImageLayerSources)
+        {
+            var zoom = tileLevel - 8; // No idea why this starts at 8 but... well.....
+            if (zoom > 0 && zoom < 18)
+                tileImageLayerSources.Add(new Uri("http://tile.openstreetmap.org/" + zoom + "/" + tilePositionX + "/" + tilePositionY + ".png"));
+        }
+    }
+}
