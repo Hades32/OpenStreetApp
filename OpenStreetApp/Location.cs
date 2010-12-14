48172b613a59eace1e50c839d50512cbf34167ae
chillta <chillta@web.de>
2010-12-14 13:06:29 +0100

    added own page for search. added location class for handling locations in geolocation resultsets and favorites

---
+++ OpenStreetApp/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OpenStreetApp
+{
+    public class Location
+    {
+        public double Longitude {get; set;}
+        public double Latitude { get; set; }
+        public String City { get; set; }
+        public String Adress { get; set; }
+        public String Area { get; set; }
+        public String Description { get; set; }
+
+        public override string ToString()
+        {
+            return "This is: " + Longitude + " and: " + Latitude;
+        }
+    }
+}
---
+++ OpenStreetApp/SearchPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace OpenStreetApp
+{
+    public partial class SearchPage : PhoneApplicationPage
+    {
+        public List<Location> Locations  { private set; get; }
+
+        public SearchPage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void buttonOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(this.TargetInput.Text))
+            {
+                //System.Diagnostics.Debug.WriteLine(Locations);
+                Locations = OSMHelpers.InputAdressToLocations(this.TargetInput.Text);
+                System.Diagnostics.Debug.WriteLine(Locations);
+            }
+        }
+
+        private void buttonAbort_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
