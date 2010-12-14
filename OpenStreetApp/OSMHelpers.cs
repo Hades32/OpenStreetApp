e9e1965d7b61ffc9d1b377f75a663a427c9c4979
chillta <chillta@web.de>
2010-12-14 17:32:03 +0100

    added second locations query

--- OpenStreetApp/OSMHelpers.cs
+++ OpenStreetApp/OSMHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Windows;
 using System.Xml.Linq;
@@ -9,40 +10,52 @@ namespace OpenStreetApp
     {
       public static void InputAdressToLocations(String inputAdressString, Action<List<Location>> callback)
         {
-            List<Location> locations = new List<Location>();
-            String encoded = System.Net.HttpUtility.UrlEncode(inputAdressString + ", *");
-            System.Net.WebClient wc = new System.Net.WebClient();
-            wc.DownloadStringCompleted += (sender, e) =>
+            InputAdressAsync(inputAdressString, (List1) =>
             {
-                XDocument xdoc = XDocument.Parse(e.Result);
-                XElement resultSetRoot = xdoc.Element("ResultSet");
-
-                var testresult = resultSetRoot.Elements("Result");
-
-                //System.Diagnostics.Debug.WriteLine(testresult);
-
-                foreach (XElement elem in testresult)
+                InputAdressAsync(inputAdressString + ",*", (List2) =>
                 {
-                    Location nl = new Location();
-                    nl.Longitude = Double.Parse(elem.Element("longitude").Value, System.Globalization.NumberFormatInfo.InvariantInfo);
-                    nl.Latitude = Double.Parse(elem.Element("latitude").Value, System.Globalization.NumberFormatInfo.InvariantInfo);
-                    nl.Line1 = elem.Element("line1").Value;
-                    nl.Line2 = elem.Element("line2").Value;
-                    nl.Line3 = elem.Element("line3").Value;
-                    nl.Line4 = elem.Element("line4").Value;
-                    // nl.City = elem.Element("city").Value;
-                    // nl.Adress = elem.Element("adress").Value;
-                    // nl.City = elem.Element("state").Value;
-                    locations.Add(nl);
-                }
-                callback(locations);
-            };
-            Uri adress = new Uri("http://where.yahooapis.com/geocode?q="
-                + encoded + "&locale=" + System.Globalization.CultureInfo.CurrentCulture.Name +
-            "&appid=dj0yJmk9ZWMzSjkwU1JWOHE0JmQ9WVdrOVF6RlpRWFp5TjJzbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZ");
-            System.Diagnostics.Debug.WriteLine(adress);
-            wc.DownloadStringAsync(adress);
+                    callback(List1.Union(List2).ToList());
+                });
+            });
         }
+
+      private static void InputAdressAsync(String inputAdressString, Action<List<Location>> callback)
+      {
+          List<Location> locations = new List<Location>();
+          String encoded = System.Net.HttpUtility.UrlEncode(inputAdressString);
+
+          System.Net.WebClient wc = new System.Net.WebClient();
+          wc.DownloadStringCompleted += (sender, e) =>
+          {
+              XDocument xdoc = XDocument.Parse(e.Result);
+              XElement resultSetRoot = xdoc.Element("ResultSet");
+
+              var testresult = resultSetRoot.Elements("Result");
+
+              //System.Diagnostics.Debug.WriteLine(testresult);
+
+              foreach (XElement elem in testresult)
+              {
+                  Location nl = new Location();
+                  nl.Longitude = Double.Parse(elem.Element("longitude").Value, System.Globalization.NumberFormatInfo.InvariantInfo);
+                  nl.Latitude = Double.Parse(elem.Element("latitude").Value, System.Globalization.NumberFormatInfo.InvariantInfo);
+                  nl.Line1 = elem.Element("line1").Value;
+                  nl.Line2 = elem.Element("line2").Value;
+                  nl.Line3 = elem.Element("line3").Value;
+                  nl.Line4 = elem.Element("line4").Value;
+                  // nl.City = elem.Element("city").Value;
+                  // nl.Adress = elem.Element("adress").Value;
+                  // nl.City = elem.Element("state").Value;
+                  locations.Add(nl);
+              }
+              callback(locations);
+          };
+          Uri adress = new Uri("http://where.yahooapis.com/geocode?q="
+              + encoded + "&locale=" + System.Globalization.CultureInfo.CurrentCulture.Name +
+          "&appid=dj0yJmk9ZWMzSjkwU1JWOHE0JmQ9WVdrOVF6RlpRWFp5TjJzbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZ");
+          System.Diagnostics.Debug.WriteLine(adress);
+          wc.DownloadStringAsync(adress);
+      }
         public static IDictionary<string, string> parseQueryParameters(string query)
         {
             var dict = new Dictionary<string, string>();
