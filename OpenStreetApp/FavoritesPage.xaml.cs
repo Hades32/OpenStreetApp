2ea8e8d0013986fd1e62f0b2b1630d68c1654f27
chillta <chillta@web.de>
2010-12-29 14:11:09 +0100

    added the lost files
    Signed-off-by: chillta <chillta@web.de>

---
+++ OpenStreetApp/FavoritesPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace OpenStreetApp
+{
+    public partial class FavoritesPage : PhoneApplicationPage
+    {
+        public FavoritesPage()
+        {
+            InitializeComponent();
+            this.DataContext = OSA_Configuration.Instance;
+        }
+
+        private void contextMenu_Opened(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("blubb");
+        }
+    }
+}
